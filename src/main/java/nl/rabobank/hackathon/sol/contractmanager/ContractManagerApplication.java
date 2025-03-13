d3831ccbaedb1344d27f5846c87f68cb1f4224b9
alexandru.palcuie <alexandru.palcuie@rabobank.nl>
2025-03-13 19:05:22 +0200

    Initial commit

---
+++ src/main/java/nl/rabobank/hackathon/sol/contractmanager/ContractManagerApplication.java
@@ -0,0 +1,13 @@
+package nl.rabobank.hackathon.sol.contractmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContractManagerApplication.class, args);
+    }
+
+}
